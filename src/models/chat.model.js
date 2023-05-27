680cd321d15ef3ce6d9c468ae07e401f96f22a54
MesachVenegas <mesach.venegas@hotmail.com>
2023-05-27 02:06:19 -0700

    create: chat sequelize model

---
+++ src/models/chat.model.js
@@ -0,0 +1,26 @@
+const { DataTypes } = require('sequelize');
+const db = require('../utils/connection');
+
+const Chat = db.define("conversations", {
+    title: {
+        type: DataTypes.STRING(100),
+        allowNull: false,
+    },
+    chatType: {
+        type: DataTypes.STRING(100),
+        allowNull: false,
+        defaultValue: 1,
+        field: "type_id"
+    },
+    createdBy: {
+        type: DataTypes.INTEGER,
+        allowNull: false,
+        field: "created_by"
+    }
+}, {
+    timestamps: true,
+    createdAt: 'created_at',
+    updatedAt: 'updated_at'
+});
+
+module.exports = Chat;
