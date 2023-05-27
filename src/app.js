6193409f374ed93db3398a3c97e72608704c83a9
MesachVenegas <mesach.venegas@hotmail.com>
2023-05-27 02:40:55 -0700

    change: import initModels

--- src/app.js
+++ src/app.js
@@ -2,6 +2,7 @@ const db = require('./utils/connection');
 const express = require('express');
 const morgan = require('morgan');
 const cors = require('cors');
+const initModels = require('./models/initModels');
 require('dotenv').config();
 
 const PORT = process.env.PORT || 8000;
@@ -11,13 +12,15 @@ app.use(cors());
 app.use(morgan('dev'));
 app.use(express.json());
 
-db.authenticate()
-    .then( () => console.log('Connection established'))
-    .catch( error => console.error(error));
+initModels();
 
-db.sync()
-    .then( () => console.log("Database synced successfully"))
-    .catch( error => console.error(error));
+db.authenticate()
+    .then(() => console.log('Connection established'))
+    .catch(error => console.error(error));
+
+db.sync({ force: true })
+    .then(() => console.log("Database synced successfully"))
+    .catch(error => console.error(error));
 
 app.listen(PORT, () => {
     console.log(`Servidor en linea y escuchando en el puerto ${PORT}`)
