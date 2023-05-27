3bc54d1419810dfb608ca693a3d360fd37ff6415
MesachVenegas <mesach.venegas@hotmail.com>
2023-05-27 13:55:43 -0700

    add: update data and avatar img routes

--- src/routes/user.routes.js
+++ src/routes/user.routes.js
@@ -1,11 +1,16 @@
 const { Router } = require('express');
-const { registerNewUser, userLogin, updateProfile } = require('../controllers/user.controller');
-const { validateUserCreate, loginValidation } = require('../validators/user.validate');
+const { registerNewUser, userLogin, updateProfile, updateAvatar } = require('../controllers/user.controller');
+const { validateUserCreate, loginValidation, updateValidation } = require('../validators/user.validate');
+const userAuth = require('../middlewares/auth.middleware');
 
 const router = Router();
 
 router.post('/users/register', validateUserCreate, registerNewUser);
 
 router.post('/users/login', loginValidation, userLogin)
+
+router.post('/users/:id', userAuth, updateValidation, updateProfile)
+
+router.post('/users/profile/avatar/:id', userAuth ,updateAvatar)
 
 module.exports = router
