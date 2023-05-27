f73ad5292dee827bcc852a138eb4d6464c1d3555
MesachVenegas <mesach.venegas@hotmail.com>
2023-05-27 02:41:54 -0700

    create: relations on models and initModel to sync with db

---
+++ src/models/initModels.js
@@ -0,0 +1,29 @@
+const ChatGroupType = require('./chatGroupType.model');
+const ChatType = require('./chatType.model');
+const Message = require('./message.model');
+const User = require('./users.model');
+const Chat = require('./chat.model');
+
+const initModels = () => {
+    // Un Chat pertenece a un usuario y un usuario tiene una o muchos Chats.
+    Chat.belongsTo(User, { foreignKey: 'createdBy'});
+    User.hasMany(Chat, { foreignKey: 'createdBy'});
+
+    // Un mensaje pertenece a un usuario y un usuario tiene uno o muchos mensajes.
+    Message.belongsTo(User, { foreignKey: 'createdBy'});
+    User.hasMany(Message, { foreignKey: 'createdBy'});
+
+    // Un Mensaje pertenece a un Chat y un Chat tiene uno o muchos mensajes.
+    Message.belongsTo(Chat, { foreignKey: 'chatId'});
+    Chat.hasMany(Message, { foreignKey: 'chatId'});
+
+    // Un tipo de chat pertenece a un chat y un chat tiene un tipo de chat.
+    ChatType.belongsTo(Chat, { foreignKey: 'chatType'});
+    Chat.hasOne(ChatType, { foreignKey: 'chatType'});
+
+    // Un Usuario pertenece a uno o muchos Chats y un Chat puede tener uno o muchos usuarios.
+    User.belongsToMany(Chat, { foreignKey: 'userId', through: ChatGroupType});
+    Chat.hasMany(User, { foreignKey: 'userId', through: UserGroupType});
+}
+
+module.exports = initModels;
