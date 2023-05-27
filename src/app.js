fb38063160d33ec6e97932423408e1bc15a14ad3
MesachVenegas <mesach.venegas@hotmail.com>
2023-05-27 02:13:01 -0700

    change: database import to connection

--- src/app.js
+++ src/app.js
@@ -1,4 +1,4 @@
-const db = require('./utils/database');
+const db = require('./utils/connection');
 const express = require('express');
 const morgan = require('morgan');
 const cors = require('cors');
