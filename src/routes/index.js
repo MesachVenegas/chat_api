8ac74c2b3600f43fafb1ee6dcd617319d201dae1
MesachVenegas <mesach.venegas@hotmail.com>
2023-05-28 15:14:49 -0700

    add: types routes to routes indexes

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,7 +1,9 @@
 const userRoutes = require('./user.routes');
+const typesRoutes = require('./types.routes');
 
 const apiRoutes = (app) => {
     app.use(userRoutes);
+    app.use(typesRoutes);
 }
 
 module.exports = apiRoutes;
