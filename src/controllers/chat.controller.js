b813e35dd076410d77802c4b2464f545858df556
MesachVenegas <mesach.venegas@hotmail.com>
2023-05-28 18:48:16 -0700

    create: chat controller

---
+++ src/controllers/chat.controller.js
@@ -0,0 +1,37 @@
+const ChatService = require('../services/chat.service');
+
+const createNewCoupleChat = async (req, res, next) => {
+    try {
+        const {title, createdBy, addressed} = req.body;
+        await ChatService.createChat({title, createdBy}, addressed);
+        res.status(201).send();
+    } catch (error) {
+        next(error);
+    }
+}
+
+const createNewGroupChat = async (req, res, next) => {
+    try {
+        const {title ,createdBy, participants} = req.body;
+        await ChatService.createGroup({title, type_id : 2 ,createdBy}, participants);
+        res.status(201).send();
+    } catch (error) {
+        next(error);
+    }
+}
+
+const getAllChatsByUser = async (req, res, next) => {
+    try {
+        const { id } = req.params;
+        const response = await ChatService.getUserChats(id);
+        res.status(200).json(response);
+    } catch (error) {
+        next(error);
+    }
+}
+
+module.exports = {
+    createNewCoupleChat,
+    createNewGroupChat,
+    getAllChatsByUser
+};
