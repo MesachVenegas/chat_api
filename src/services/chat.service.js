910214b894ac79cfbaa2d0dffe6ab9ebe8846e6c
MesachVenegas <mesach.venegas@hotmail.com>
2023-05-29 06:33:24 -0700

    change: remove model ChatGroup and use association method

--- src/services/chat.service.js
+++ src/services/chat.service.js
@@ -1,4 +1,3 @@
-const ChatGroupType = require('../models/chatGroupType.model');
 const ChatType = require('../models/chatType.model');
 const Message = require('../models/message.model');
 const User = require('../models/users.model');
@@ -8,15 +7,9 @@ class ChatService {
 
     static async createChat(data, addressed) {
         try {
-            console.log(data);
             const chat = await Chat.create(data);
-            const message = [
-                { chatId: chat.id, userId: chat.createdBy },
-                { chatId: chat.id, userId: addressed }
-            ]
-            await Promise.all(message.map(async data => {
-                await ChatGroupType.create(data)
-            }))
+            const participant = await User.findByPk(addressed)
+            chat.addUser(participant);
             return chat;
         } catch (error) {
             throw error;
@@ -26,49 +19,65 @@ class ChatService {
     static async createGroup(data, participants) {
         try {
             const groupChat = await Chat.create(data);
-
-            const message = [
-                { chatId: groupChat.id, userId: groupChat.createdBy },
-            ]
-
-            participants.forEach(participant => {
-                message.push({ chatId: groupChat.id, userId: participant})
-            })
-            console.log(message);
-
-            await Promise.all(message.map(async data => {
-                await ChatGroupType.create(data)
-            }))
-
             return groupChat;
         } catch (error) {
             throw error;
         }
     }
 
+
     static async getUserChats (id) {
         try {
             const chats = await Chat.findAll({
                 where: { createdBy: id },
                 attributes: {
-                    exclude: ['createdBy', 'chatType'],
+                    exclude: ['createdBy', 'typeId'],
                 },
-                include: [
-                    {
-                        model: ChatType
-                    },
-                    {
-                        model: User,
-                        attributes: {
-                            exclude: ['password']
-                        }
-                    },
-                    {
-                        model: Message
+                include:{
+                    model: ChatType,
+                    attributes: {
+                        exclude: ['id']
                     }
-                ]
+                }
             })
             return chats;
+        } catch (error) {
+            throw error;
+        }
+    }
+
+    static async delete(id){
+        try {
+            const result = Chat.destroy({ where: id})
+            return result;
+        } catch (error) {
+            throw error;
+        }
+    }
+
+    static async getChatParticipants(id) {
+        try {
+            const chat = await Chat.findByPk(id, {
+                attributes: { exclude: ['createdBy', 'typeId']},
+                include: [
+                    {
+                        model: User,
+                        attributes: { exclude: ['password']},
+                        // through: { attributes: []}
+                    },
+                    {
+                        model: Message,
+                        attributes: { exclude: ['createdBy', 'chatId']},
+                        include: [
+                            {
+                                model: User,
+                                attributes: { exclude: ['password'] }
+                            }
+                        ]
+                    },
+                ]
+            });
+            return chat;
         } catch (error) {
             throw error;
         }
