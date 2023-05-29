9232bf1d8d88400c614b565d73e8dd2d1a8db059
MesachVenegas <mesach.venegas@hotmail.com>
2023-05-28 18:47:18 -0700

    add: chat routes

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,9 +1,11 @@
 const userRoutes = require('./user.routes');
 const typesRoutes = require('./types.routes');
+const chatRoutes = require('./chat.routes');
 
 const apiRoutes = (app) => {
     app.use(userRoutes);
     app.use(typesRoutes);
+    app.use(chatRoutes);
 }
 
 module.exports = apiRoutes;
